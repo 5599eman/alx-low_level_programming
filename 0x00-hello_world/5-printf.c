9399d192c0df5d71654209fd124e0d39a11d9166
5599eman <emanmohamedalmahdy@gmail.com>
2023-06-18 16:51:36 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main - A program to print a line using printf function
+ * Return: 0 (Success)
+ */ 
+int main(viod)
+{
+	Print("with proper grammar,but the outcome is a piece of art,\n");
+	return (0);
+}
