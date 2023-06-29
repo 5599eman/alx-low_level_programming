ac28027e052823b4d25b3d965ea1de1326f15e40
5599eman <emanmohamedalmahdy@gmail.com>
2023-06-29 07:09:16 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -13,14 +13,15 @@ void print_number(int n)
 	/*First check if its negtive*/
 	if (n < 0)
 	{
-		_putchar('_');
-		num = -unm;
+		_putchar('-');
+		num = -num;
 	}
 
 	/*print the first few digits*/
 	if ((num / 10) > 0)
-		print_number(num / 10)
+		print_number(num / 10);
 
 			/*print the last digit*/
+
 			_putchar((num % 10) + 48);
 }
